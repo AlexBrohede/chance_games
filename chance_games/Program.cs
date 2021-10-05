1f10dc22229d1b45027361ee042746594bcc4e0a
AlexBrohede <alexbrohede@gmail.com>
2021-10-05 16:39:36 +0200

    Added a few random numbers

--- chance_games/Program.cs
+++ chance_games/Program.cs
@@ -6,7 +6,14 @@ namespace chance_games
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Testing GitHub connectivity...");
+            Random rnd = new Random();
+            int dice = rnd.Next(1, 7);   // creates a number between 1 and 6
+            int card = rnd.Next(52);     // creates a number between 0 and 51
+            int bjcard = rnd.Next(1,11);     // creates a number between 1 and 10
+
+            Console.WriteLine("Dice: "+dice);
+            Console.WriteLine("Card: " + card);
+            Console.WriteLine("Black Jack Card: " + bjcard);
         }
     }
 }
