c9ef0e167bcf6f081224864ac2ed244c61d7203d
AlexBrohede <alexbrohede@gmail.com>
2021-10-05 17:01:35 +0200

    added basic menu

--- chance_games/Program.cs
+++ chance_games/Program.cs
@@ -9,11 +9,24 @@ namespace chance_games
             Random rnd = new Random();
             int dice = rnd.Next(1, 7);   // creates a number between 1 and 6
             int card = rnd.Next(52);     // creates a number between 0 and 51
-            int bjcard = rnd.Next(1,11);     // creates a number between 1 and 10
+            int bjcard = rnd.Next(1, 11);     // creates a number between 1 and 10
 
-            Console.WriteLine("Dice: "+dice);
-            Console.WriteLine("Card: " + card);
-            Console.WriteLine("Black Jack Card: " + bjcard);
+            Console.WriteLine("1 Dice \n2 Card \n3 BJcard");
+            int Choice = int.Parse(Console.ReadLine());
+
+            if (Choice == 1)
+            {
+                Console.WriteLine("Dice: " + dice);
+            }
+            else if (Choice == 2)
+            {
+                Console.WriteLine("Card: " + card);
+            }
+            else if (Choice == 3)
+            {
+                Console.WriteLine("Black Jack Card: " + bjcard);
+            }
+            else { Console.WriteLine($"{Choice} was not a option!"); }
         }
     }
 }
