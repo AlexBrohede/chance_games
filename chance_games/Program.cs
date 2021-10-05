e9b94fc4445e8231bbc2c405247482a051f00595
AlexBrohede <alexbrohede@gmail.com>
2021-10-05 16:27:44 +0200

    Initial check-in of module chance_games

---
+++ chance_games/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chance_games
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
