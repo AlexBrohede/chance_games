e5535dbb8ad70e76f104fb4ed1b8c71024e2f74a
AlexBrohede <alexbrohede@gmail.com>
2021-10-05 16:35:29 +0200

    New console msg

--- chance_games/Program.cs
+++ chance_games/Program.cs
@@ -6,7 +6,7 @@ namespace chance_games
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Testing GitHub connectivity...");
         }
     }
 }
